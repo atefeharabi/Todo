d07be48122a68996bafbe282163a26f4afa3a22a
atefeharabi <atefeh.araabi@gmail.com>
2023-02-04 17:38:40 +0330

    009: add delete button for deleting an object from database

--- Home/urls.py
+++ Home/urls.py
@@ -5,4 +5,5 @@ from .views import *
 urlpatterns = [
     path('', home, name='home'),
     path('detail/<int:todo_id>/', detail, name='details'),
+    path('delete/<int:todo_id>/', delete, name='delete'),
 ]
