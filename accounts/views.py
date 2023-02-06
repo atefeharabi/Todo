03b63f2f9af293ba13e129e5c65b9cdee4fbf1fc
atefeharabi <atefeh.araabi@gmail.com>
2023-02-06 22:33:42 +0330

    0018: user logout is added to project

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from .forms import UserRegisterationForm, UserLoginForm
 
 
@@ -33,3 +33,9 @@ def user_login(request):
     else:
         form = UserLoginForm()
         return render(request, 'login.html', {'form': form})
+
+
+def user_logout(request):
+    logout(request)
+    messages.success(request, 'logged out successfully', 'success')
+    return redirect('home')
