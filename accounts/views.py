afc4194f123b0d67c09e6a9a22a3a4baf43ce6e6
atefeharabi <atefeh.araabi@gmail.com>
2023-02-06 20:03:42 +0330

    015: url of accounts app was corrected

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 
-# Create your views here.
+
+def user_register(request):
+    return HttpResponse('hello')
